5c62e7518e505ec840b1b24f5b1abfce334c6246
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-03-28 13:42:46 -0700

    Create the web server in a new index.js file.

---
+++ index.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import express from 'express';
+import routes from './itemRoutes';
+
+// const http = require("http");
+const app = express();
+app.use(express.json());
+
+const hostname = "127.0.0.1";
+const port = 4000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/itemDB', {
+  "useNewUrlParser": true,
+  "useUnifiedTopology": true
+});
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('<h1>Hello World!</h1><h2>Item List</h2><ul><li style="color: red">Item 1</li></ul><h2>Add Item</h2><form><label for="item">Item: </label><input type="text"></br><input type="submit" value="Submit"></form>');
+// });
+
+routes(app);
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
